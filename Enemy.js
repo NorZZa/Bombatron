8a361b12e33ebd2237ce06cb75a3a41619ea2bf4
Zhenmeister <Zhen.Then@gmail.com>
2015-10-14 20:29:45 +1100

    Update Enemy.js

--- Enemy.js
+++ Enemy.js
@@ -1,65 +1,70 @@
-
-
-
- 
-var Enemy = function(x, y)
+var Enemy = function(x, y) 
 {
+	this.sprite = new Sprite("bat.png");
+	this.sprite.buildAnimation(2, 1, 88, 94, 0.3, [0,1]);
+	this.sprite.setAnimationOffset(0, -35, -40);
 	
-// Still need to adjust still image here.
+	this.position = new Vector2();
+	this.position.set(x, y);
+		
+	this.velocity = new Vector2();
+	
+	this.moveRight = true;
+	this.pause = 0;
 };
-
-
 
 Enemy.prototype.update = function(deltaTime)
 {
 	this.sprite.update(deltaTime);
-
+	
 	if(this.pause > 0)
 	{
-		this.pause -= dt;
+		this.pause -= deltaTime;
 	}
 	else
 	{
-		var ddx = 0; 		//acceleration
-
+		var ddx = 0;
+		
 		var tx = pixelToTile(this.position.x);
 		var ty = pixelToTile(this.position.y);
-		var nx = (this.position.x)%TILE;	//True if enemy overlaps right
-		var ny = (this.position.y)%TILE;	//True if enemey overlaps below
+		var nx = (this.position.x)% TILE;
+		var ny = (this.position.y)% TILE;
 		var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
 		var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
 		var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
 		var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
-
+		//Right
 		if(this.moveRight)
 		{
-			if(celldiag && !cellright) {
-				ddx = ddx + ENEMY_ACCEL;	//enemy wants to go right
-
-
+			if(celldiag && !cellright)
+			{
+				ddx = ddx + ENEMY_ACCEL;
 			}
-			else{
+			else
+			{
 				this.velocity.x = 0;
 				this.moveRight = false;
 				this.pause = 0.5;
-			}
+			}		
 		}
-
+		//Left
 		if(!this.moveRight)
 		{
-			if(celldown && !cell) {
-				ddx = ddy - ENEMY_ACCEL;	//enemy wants to go left
+			if(celldown && !cell)
+			{
+				ddx = ddx - ENEMY_ACCEL;
 			}
-			else {
+			else
+			{
 				this.velocity.x = 0;
 				this.moveRight = true;
 				this.pause = 0.5;
 			}
 		}
-
+		
 		this.position.x = Math.floor(this.position.x + (deltaTime * this.velocity.x));
-		this.velocity.x = bound(this.velocity.x + (deltaTime * ddx),
-													-ENEMY_MAXDX, ENEMY_MAXDX);
+		this.velocity.x = bound(this.velocity.x + (deltaTime * ddx), -ENEMY_MAXDX, ENEMY_MAXDX);
+
 	}
 }
 
